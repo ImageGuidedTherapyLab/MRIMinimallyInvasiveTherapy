6b54227f8a5e2e0cda63bbbb374a8a02aba619ff
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2009-01-22 05:43:04 +0000

    parallel image processing strategy  * partition w/ petsc DA  * read full image on each proc  * use petsc partition to get a subdomain of the region with RegionOfInterestImageFilter  * use GetBufferPointer w/ the RegionOfInterestImageFilter to pass pixel buffer to petsc to take advantage of DA mpi data struc for ghost cells etc...  (this assumes that the filter copies the particular image buffer to its own continuous array )
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/trunk/realtime@3001 abaf0659-affb-4052-b7bf-27aea6606fda

--- DicomReadImagRealWriteTemp.cxx
+++ DicomReadImagRealWriteTemp.cxx
@@ -86,14 +86,17 @@
 #include "itkImageSliceIteratorWithIndex.h"
 // Software Guide : EndCodeSnippet
 
-
-
 #include <vector>
 #include <itksys/SystemTools.hxx>
 #include <boost/lexical_cast.hpp>
 
+// petsc includes
+#include "petscda.h"
+#include "petscsys.h"
+
 
 // globals 
+static char help[] = "Tests DAGetInterpolation for nonuniform DA coordinates.\n\n";
 const unsigned int          Dimension = 3;
 const double                       pi = 3.1415926535897931;
 // useful typedefs
@@ -245,6 +248,60 @@ void WriteImage(InputImageType::Pointer Image, BufferIteratorType imageIt,
     }
   return;
 }
+
+// main driver routine
+#undef __FUNCT__
+#define __FUNCT__ "daroutine"
+int daroutine(int argc,char **argv)
+{
+  PetscInt       M = 5,N = 4,P = 3, m = PETSC_DECIDE,n = PETSC_DECIDE,p = PETSC_DECIDE,dim = 1;
+  PetscErrorCode ierr;
+  DA             dac,daf;
+  DAPeriodicType ptype = DA_NONPERIODIC;
+  DAStencilType  stype = DA_STENCIL_BOX;
+  Mat            A;
+
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);CHKERRQ(ierr); 
+
+  /* Read options */
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-M",&M,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-N",&N,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-P",&P,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-m",&m,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-n",&n,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-p",&p,PETSC_NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetInt(PETSC_NULL,"-dim",&dim,PETSC_NULL);CHKERRQ(ierr);
+
+  /* Create distributed array and get vectors */
+  if (dim == 1) {
+    ierr = DACreate1d(PETSC_COMM_WORLD,ptype,M,1,1,PETSC_NULL,&dac);CHKERRQ(ierr);
+  } else if (dim == 2) {
+    ierr = DACreate2d(PETSC_COMM_WORLD,ptype,stype,M,N,PETSC_DECIDE,PETSC_DECIDE,1,1,PETSC_NULL,PETSC_NULL,&dac);CHKERRQ(ierr);
+  } else if (dim == 3) {
+    ierr = DACreate3d(PETSC_COMM_WORLD,ptype,stype,M,N,P,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,1,1,PETSC_NULL,PETSC_NULL,PETSC_NULL,&dac);CHKERRQ(ierr);
+  }
+
+  ierr = DARefine(dac,PETSC_COMM_WORLD,&daf);CHKERRQ(ierr);
+
+  ierr = DASetUniformCoordinates(dac,0.0,1.0,0.0,1.0,0.0,1.0);CHKERRQ(ierr);
+  if (dim == 1) {
+   // ierr = SetCoordinates1d(daf);CHKERRQ(ierr);
+  } else if (dim == 2) {
+   // ierr = SetCoordinates2d(daf);CHKERRQ(ierr);
+  } else if (dim == 3) {
+   // ierr = SetCoordinates3d(daf);CHKERRQ(ierr);
+  }
+  ierr = DAGetInterpolation(dac,daf,&A,0);CHKERRQ(ierr);
+
+
+  /* Free memory */
+  ierr = DADestroy(dac);CHKERRQ(ierr);
+  ierr = DADestroy(daf);CHKERRQ(ierr);
+  ierr = MatDestroy(A);CHKERRQ(ierr);
+  ierr = PetscFinalize();CHKERRQ(ierr);
+  return 0;
+}
+ 
 // main driver routine
 int main( int argc, char* argv[] )
 {
--- makefile
+++ makefile
@@ -1,3 +1,5 @@
+include ${PETSC_DIR}/bmake/common/base
+
 .PHONY: tags
 
 all: Makefile
@@ -7,4 +9,40 @@ Makefile: CMakeLists.txt
 tags:
 	ctags --langmap=c++:+.txx --languages=c++ -R $(ITK_SOURCE) CImg.h *
 
+ITK_CXXFLAGS=-ftemplate-depth-50 
+# taken from cmake with verbose makefile
+ITK_INCLUDE= -I$(ITK_HOME)/include/InsightToolkit/gdcm/src \
+             -I$(ITK_HOME)/include/InsightToolkit/gdcm \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/vxl/core \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/vxl/vcl \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/vxl/v3p/netlib \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/itkExtHdrs \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/nifti/znzlib \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/nifti/niftilib \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/expat \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/DICOMParser \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/NrrdIO \
+             -I$(ITK_HOME)/include/InsightToolkit/Utilities/MetaIO \
+             -I$(ITK_HOME)/include/InsightToolkit/SpatialObject \
+             -I$(ITK_HOME)/include/InsightToolkit/Numerics/NeuralNetworks \
+             -I$(ITK_HOME)/include/InsightToolkit/Numerics/Statistics \
+             -I$(ITK_HOME)/include/InsightToolkit/Numerics/FEM \
+             -I$(ITK_HOME)/include/InsightToolkit/IO \
+             -I$(ITK_HOME)/include/InsightToolkit/Numerics \
+             -I$(ITK_HOME)/include/InsightToolkit/Common \
+             -I$(ITK_HOME)/include/InsightToolkit/BasicFilters \
+             -I$(ITK_HOME)/include/InsightToolkit/Algorithms \
+             -I$(ITK_HOME)/include/InsightToolkit  
+ITK_LIB= -rdynamic -L$(ITK_DIR) -lITKFEM -lITKIO -lITKStatistics -lITKNumerics -lITKBasicFilters -lITKNrrdIO -litkgdcm -litkjpeg12 -litkjpeg16 -litkopenjpeg -luuid -litkpng -litktiff -litkjpeg8 -lITKSpatialObject -lITKMetaIO -lITKDICOMParser -lITKEXPAT -lITKniftiio -lITKznz -litkzlib -lITKCommon -litkvnl_inst -litkvnl_algo -litkv3p_netlib -litkvnl -litkvcl -lm -litksys -lpthread -ldl -lm -Wl,-rpath,$(ITK_DIR)
+#
+INCLUDE= $(PETSC_INCLUDE) $(ITK_INCLUDE) 
 
+%.o: %.cxx
+	@echo Compiling $<
+	$(PCC) $(PREC_FLAGS) $(PCC_FLAGS) $(ITK_CXXFLAGS) $(INCLUDE) -o $@ -c $<
+
+
+#DicomReadImagRealWriteTemp: DicomReadImagRealWriteTemp.cxx chkopts
+DicomReadImagRealWriteTemp: DicomReadImagRealWriteTemp.o chkopts
+	-${CLINKER} -o DicomReadImagRealWriteTemp DicomReadImagRealWriteTemp.o  ${PETSC_DM_LIB} ${ITK_LIB} 
