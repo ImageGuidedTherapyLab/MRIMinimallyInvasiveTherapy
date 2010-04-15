a9f7437270107382e690f9fc97dd6837dce81929
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2010-04-15 17:34:52 +0000

    move to a standard numbering system...
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/trunk/realtime@4304 abaf0659-affb-4052-b7bf-27aea6606fda

---
+++ headerInfoSort.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import ConfigParser
+#the jobid is passed in from the command line
+controlfile = sys.argv[1]
+#controlfile = "/mnt/data/scratch/fuentes/DDDAS/data/biotex/090318_751642_treat/controlBrain.ini"
+
+#read the main control file
+iniFile  = ConfigParser.ConfigParser()
+iniFile.readfp( open("%s" % controlfile , "r") )
+
+# get exampath info
+ExamPath   = iniFile.get(   "mrti" ,"exampath"  )
+dirID      = iniFile.getint("mrti" ,"dirid"     )
+dictionary = iniFile.get(   "mrti" ,"dictionary")
+instanceTag = "0020|0013"
+# TODO wrap c++ code to return  value directly
+printTagExe = "/home/dfuentes/DDDAS/trunk/utilities/DicomImageReadReturnIntTag"
+
+# make new directory
+if(not os.path.isdir("s%dSorted" % dirID)):
+  os.mkdir("s%dSorted" % dirID)
+# TODO add realtime abilities as data is written to the ExamPath
+for file in os.listdir( "%s/s%d" % (ExamPath,dirID) ):
+    dicomFile = "%s/s%d/%s" % (ExamPath,dirID,file) 
+    fin,fout = os.popen4( "%s %s '%s' %s 2> /dev/null " % (printTagExe,dicomFile,instanceTag,dictionary) )
+    result = fout.read()
+    timeid = int(result.split("=")[1])
+    print  "%s %d" % (file,timeid) 
+    os.system( "cp %s s%dSorted/i%d.MRDC.%d" % (dicomFile,dirID, dirID + timeid,timeid ) )
