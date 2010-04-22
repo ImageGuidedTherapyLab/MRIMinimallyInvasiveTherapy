9a965dba0f25cf3f36a77512fbb50561faf65513
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2010-04-22 20:20:40 +0000

    should be ready...
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/trunk/realtime@4318 abaf0659-affb-4052-b7bf-27aea6606fda

--- headerInfoSort.py
+++ headerInfoSort.py
@@ -10,21 +10,54 @@ iniFile  = ConfigParser.ConfigParser()
 iniFile.readfp( open("%s" % controlfile , "r") )
 
 # get exampath info
-ExamPath   = iniFile.get(   "mrti" ,"exampath"  )
-dirID      = iniFile.getint("mrti" ,"dirid"     )
-dictionary = iniFile.get(   "mrti" ,"dictionary")
+ExamPath   = iniFile.get(   "mrti" ,"exampath"    )
+magID      = iniFile.getint("mrti" ,"magnitudeid" )
+phaseID    = iniFile.getint("mrti" ,"phaseid"     )
+dictionary = iniFile.get(   "mrti" ,"dictionary"  )
 instanceTag = "0020|0013"
+
 # TODO wrap c++ code to return  value directly
 printTagExe = "/home/dfuentes/DDDAS/trunk/utilities/DicomImageReadReturnIntTag"
+try:
+  DicomToComplex = iniFile.get(   "compexec" ,"Dicomtocomplex"  )
+except ConfigParser.NoOptionError: 
+  DicomToComplex = "~/DDDAS/trunk/utilities/DicomReadWriteComplexImage"
 
 # make new directory
-os.system('mkdir -p %s/Sorted/s%d' % (ExamPath,dirID) )
+os.system('mkdir -p %s/Sorted/s%d' % (ExamPath,magID) )
+
+magnitudeList = os.listdir( "%s/s%d" % (ExamPath, magID ) )
+numFiles = len(magnitudeList) 
+phaseList     = os.listdir( "%s/s%d" % (ExamPath,phaseID) ) 
+try:
+   assert numFiles == len(phaseList)     
+except AssertionError:
+   raise IndexError("\n\n    error with number of files in the directory " )
+
+# set output to magnitude id
+dirID = magID
 
 # TODO add realtime abilities as data is written to the ExamPath
-for file in os.listdir( "%s/s%d" % (ExamPath,dirID) ):
-    dicomFile = "%s/s%d/%s" % (ExamPath,dirID,file) 
-    fin,fout = os.popen4( "%s %s '%s' %s 2> /dev/null " % (printTagExe,dicomFile,instanceTag,dictionary) )
+for magnitudeFile,phaseFile in zip(magnitudeList,phaseList): 
+    # copy magnitude first
+    dicomMagFile   = "%s/s%d/%s" % (ExamPath, magID ,magnitudeFile) 
+    fin,fout = os.popen4( "%s %s '%s' %s 2> /dev/null " % (printTagExe,dicomMagFile,instanceTag,dictionary) )
     result = fout.read()
-    timeid = int(result.split("=")[1])
-    print  "%s %d" % (file,timeid) 
-    os.system( "cp %s %s/Sorted/s%d/i%d.MRDC.%d" % (dicomFile,ExamPath,dirID,dirID+timeid,timeid) )
+    magTimeID = int(result.split("=")[1])
+    os.system( "cp %s %s/Sorted/s%d/i%d.MRDC.%d" % (dicomMagFile,ExamPath,dirID,dirID+2*magTimeID-1,2*magTimeID-1) )
+    # now copy phase
+    dicomPhaseFile = "%s/s%d/%s" % (ExamPath,phaseID,  phaseFile  ) 
+    fin,fout = os.popen4( "%s %s '%s' %s 2> /dev/null " % (printTagExe,dicomPhaseFile,instanceTag,dictionary) )
+    result = fout.read()
+    phaseTimeID = int(result.split("=")[1])
+    os.system( "cp %s %s/Sorted/s%d/i%d.MRDC.%d" % (dicomPhaseFile,ExamPath,dirID,dirID+2*phaseTimeID,2*phaseTimeID) )
+    print  "%s %d %s %d " % (magnitudeFile,magTimeID,phaseFile,phaseTimeID) 
+
+
+# one based
+for fileid in range(1,numFiles+1):
+    os.system( "%s -M%s/Sorted/s%d/i%d.MRDC.%d -P%s/Sorted/s%d/i%d.MRDC.%d --output %s/Sorted/s%d/image -timeid %d" % ( 
+                  DicomToComplex ,
+                      ExamPath,dirID,dirID+2*fileid-1,2*fileid-1, 
+                      ExamPath,dirID,dirID+2*fileid  ,2*fileid  , 
+                      ExamPath,dirID,fileid-1 )  ) 
