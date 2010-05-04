c50a29dea131e28f2c277ad2225b5bf669cccee0
fuentes <fuentes@abaf0659-affb-4052-b7bf-27aea6606fda>
2010-05-04 23:54:34 +0000

    setting up python class for GE and siemens data acquistion...
    git-svn-id: https://svn.ices.utexas.edu/repos/dddas/codes/trunk/realtime@4346 abaf0659-affb-4052-b7bf-27aea6606fda

--- headerInfoSort.py
+++ headerInfoSort.py
@@ -8,32 +8,32 @@ import itkUtilities # custom python module for access to itk library
 #http://docs.python.org/library/sets.html
 from sets import Set
 
-#the jobid is passed in from the command line
-controlfile = sys.argv[1]
-#controlfile = "/mnt/data/scratch/fuentes/DDDAS/data/biotex/090318_751642_treat/controlBrain.ini"
+class AcquisitionBaseClass:
+  def __init__(self,ControlFile):
+    #read the main control file
+    self.iniFile  = ConfigParser.ConfigParser()
+    self.iniFile.readfp( open("%s" % ControlFile , "r") )
+    self.instanceTag = "0020|0013"
+    #get dicom Dictionary
+    try:
+      self.dictionary = self.iniFile.get(   "mrti" ,"dictionary"  )
+    except ConfigParser.NoOptionError:
+      raise IOError("\n\n    No Dicom Dictionary FOUND! " )
+    # get exampath info
+    self.ExamPath = self.iniFile.get(   "mrti" ,"exampath"    )
+    # Set C++ executable to get header data info
+    try:
+      self.DicomToComplex = self.iniFile.get(   "compexec" ,"Dicomtocomplex"  )
+    except ConfigParser.NoOptionError: 
+      self.DicomToComplex = "~/DDDAS/trunk/utilities/DicomReadWriteComplexImage"
+  def GetHeaderInfo(self):
+    # loop over possible slice number tags to find the slice location
+    for sliceIDKey in ["0021|104f"]:
+      nslice = int( itkUtilities.GetDicomTag( dicomMagFile, sliceIDKey ,
+                                                            self.dictionary) )
 
-#read the main control file
-iniFile  = ConfigParser.ConfigParser()
-iniFile.readfp( open("%s" % controlfile , "r") )
 
-#get dicom Dictionary
-try:
-  dictionary= iniFile.get(   "mrti" ,"dictionary"  )
-except ConfigParser.NoOptionError:
-  raise IOError("\n\n    No Dicom Dictionary FOUND! " )
-instanceTag = "0020|0013"
-
-# get exampath info
-ExamPath   = iniFile.get(   "mrti" ,"exampath"    )
-
-# Set C++ executable to get header data info
-try:
-  DicomToComplex = iniFile.get(   "compexec" ,"Dicomtocomplex"  )
-except ConfigParser.NoOptionError: 
-  DicomToComplex = "~/DDDAS/trunk/utilities/DicomReadWriteComplexImage"
-
-# get some initial header info from the first file...
-try: 
+class SiemensAquisitionGEWrite(AcquisitionBaseClass):
   # try building list of magnitude and phase data
   magnID   = iniFile.getint("mrti" ,"magnitudeid" )
   phasID   = iniFile.getint("mrti" ,"phaseid"     )
@@ -46,7 +46,30 @@ try:
      raise IndexError("\n\n    error with number of files in the directory " )
   # set output to magnitude id
   dirID = magnID
-except ConfigParser.NoOptionError:
+  # TODO add realtime abilities as data is written to the ExamPath
+  for magnitudeFile,phaseFile in zip(fileListOne,fileListTwo): 
+      # copy magnitude first
+      dicomMagFile   = "%s/s%d/%s" % (ExamPath, magnID ,magnitudeFile) 
+      magTimeID = int( itkUtilities.GetDicomTag( dicomMagFile, instanceTag,
+                                                                dictionary) )
+      os.system( "cp %s %s/Processed/s%d/i%d.MRDC.%d" % (dicomMagFile,ExamPath,dirID,dirID+2*magTimeID-1,2*magTimeID-1) )
+      # now copy phase
+      dicomPhaseFile = "%s/s%d/%s" % (ExamPath,phasID,  phaseFile  ) 
+      phaseTimeID = int( itkUtilities.GetDicomTag( dicomPhaseFile, instanceTag,
+                                                                   dictionary) )
+      os.system( "cp %s %s/Processed/s%d/i%d.MRDC.%d" % (dicomPhaseFile,ExamPath,dirID,dirID+2*phaseTimeID,2*phaseTimeID) )
+      print  "%s %d %s %d " % (magnitudeFile,magTimeID,phaseFile,phaseTimeID) 
+   
+   
+   # one based
+   #for fileid in range(1,numFiles+1):
+   #    os.system( "%s -M%s/Processed/s%d/i%d.MRDC.%d -P%s/Processed/s%d/i%d.MRDC.%d --output %s/Processed/s%d/image -timeid %d" % ( 
+   #                  DicomToComplex ,
+   #                      ExamPath,dirID,dirID+2*fileid-1,2*fileid-1, 
+   #                      ExamPath,dirID,dirID+2*fileid  ,2*fileid  , 
+   #                      ExamPath,dirID,fileid-1 )  ) 
+
+class GEAquisitionGEWrite(AcquisitionBaseClass):
   dirID    = iniFile.getint("mrti" ,"dirid"     )
   # try building list of real and imaginary data as default
   realID   = iniFile.getint("mrti" ,"realid"      )
@@ -56,35 +79,33 @@ except ConfigParser.NoOptionError:
   except AssertionError:
      raise IndexError("\n\n    i have never encountered real and imaginary data in different directories... " )
 
-# make new directory
-os.system('mkdir -p %s/Processed/s%d' % (ExamPath,dirID) )
-
-
-# typically assume a high number of images to read in 
-ntime  = iniFile.getint("mrti","ntime")
-timeID = 0 
-#while (timeID < ntime): 
-
-
-# TODO add realtime abilities as data is written to the ExamPath
-for magnitudeFile,phaseFile in zip(fileListOne,fileListTwo): 
-    # copy magnitude first
-    dicomMagFile   = "%s/s%d/%s" % (ExamPath, magnID ,magnitudeFile) 
-    magTimeID = int( itkUtilities.GetDicomTag( dicomMagFile, instanceTag,
-                                                              dictionary) )
-    os.system( "cp %s %s/Processed/s%d/i%d.MRDC.%d" % (dicomMagFile,ExamPath,dirID,dirID+2*magTimeID-1,2*magTimeID-1) )
-    # now copy phase
-    dicomPhaseFile = "%s/s%d/%s" % (ExamPath,phasID,  phaseFile  ) 
-    phaseTimeID = int( itkUtilities.GetDicomTag( dicomPhaseFile, instanceTag,
-                                                                 dictionary) )
-    os.system( "cp %s %s/Processed/s%d/i%d.MRDC.%d" % (dicomPhaseFile,ExamPath,dirID,dirID+2*phaseTimeID,2*phaseTimeID) )
-    print  "%s %d %s %d " % (magnitudeFile,magTimeID,phaseFile,phaseTimeID) 
-
-
-# one based
-#for fileid in range(1,numFiles+1):
-#    os.system( "%s -M%s/Processed/s%d/i%d.MRDC.%d -P%s/Processed/s%d/i%d.MRDC.%d --output %s/Processed/s%d/image -timeid %d" % ( 
-#                  DicomToComplex ,
-#                      ExamPath,dirID,dirID+2*fileid-1,2*fileid-1, 
-#                      ExamPath,dirID,dirID+2*fileid  ,2*fileid  , 
-#                      ExamPath,dirID,fileid-1 )  ) 
+#single file use
+if __name__ == "__main__":
+   #ini file passed in from the command line
+   #controlfile = "/mnt/data/scratch/fuentes/DDDAS/data/biotex/090318_751642_treat/controlBrain.ini"
+   controlfile = sys.argv[1] 
+   
+   #initialize
+   try: 
+     # try Siemens Acquisition w/ GE Header
+     fileProcess = SiemensAquisitionGEWrite(controlfile) 
+   except ConfigParser.NoOptionError:
+     # try GE Acquisition w/ GE Header
+     fileProcess = GEAquisitionGEWrite(controlfile) 
+   
+   # get some initial header info from the first file...
+   fileProcess.GetHeaderInfo()
+   
+   # make new directory
+   os.system('mkdir -p %s/Processed/s%d' % (fileProcess.ExamPath,
+                                            fileProcess.dirID) )
+   
+   # typically assume a high number of images to read in 
+   ntime  = fileProcess.iniFile.getint("mrti","ntime")
+   timeID = 0 
+   while (timeID < ntime): 
+      fileProcess.updateDirectoryInfo()
+      # parse the new files found, convert to complex format,
+      # and update the timeID
+      fileProcess.parseAndWriteAsComplexData()
+   
